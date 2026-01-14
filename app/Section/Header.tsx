b0ba09f7159b696e7c8d53c8baa6c603f02aed11
jvcopo <jacopo.benedetti@devergence.tech>
2026-01-14 16:45:42 +0100

    Bug Fix

--- app/Section/Header.tsx
+++ app/Section/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return(
         <div className="flex flex-col items-center justify-center w-full p-12 gap-4 mt-4">
             <h1 className="text-6xl text-white font-semibold">Pathfinding Visualizer</h1>
-            <p className="text-sm text-(--text-light) font-sans">Visualizza gli algoritmi di ricerca del percorso in tempo reale</p>
+            <p className="text-sm text-(--text-light) font-sans">Visualize pathfinding algorithms in real time</p>
         </div>
     );
 }
